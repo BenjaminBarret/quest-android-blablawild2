57295cf5dc536b9e83a735f0f52fc7107b5487b1
Benjamin Barret <benjaminbarret.ps4@gmail.com>
2018-04-16 10:38:09 +0200

    Firebase

--- app/src/main/java/fr/wildcodeschool/blablawild2/ItinerarySearchActivity.java
+++ app/src/main/java/fr/wildcodeschool/blablawild2/ItinerarySearchActivity.java
@@ -8,6 +8,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ItinerarySearchActivity extends AppCompatActivity {
 
     public static final String EXTRA_TRIP = "EXTRA_TRIP";
@@ -36,6 +42,14 @@ public class ItinerarySearchActivity extends AppCompatActivity {
                 } else {
                     Intent intent = new Intent(ItinerarySearchActivity.this, ItineraryListActivity.class);
                     TripModel tripModel = new TripModel(departure, destination, date);
+
+                    // TODO : save itineraryModel into Firebase
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference tripsRef = database.getReference("trips");
+                    String key = tripsRef.push().getKey();
+                    // sauvegarde la valeur
+                    tripsRef.child(key).setValue(tripModel);
 
                     intent.putExtra(EXTRA_TRIP, tripModel);
                     startActivity(intent);
